32b567128f0ee7765648db31404bb1eefef2bd6c
zhaofelix <15021179915@163.com>
2020-10-26 10:12:15 +0800

    提交

---
+++ src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
