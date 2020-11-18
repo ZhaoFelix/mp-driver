330b8ce40db37e250e07281f1062220004f47e4a
tollenceld <Tollenceld@icloud.com>
2020-11-18 23:48:04 +0800

    更新

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="main-container">
-    
     <div class="course-list">
-      <div v-for="i in 5" :key="i" class="course-card">
+      <div v-for="i in 3" :key="i" class="course-card">
         <div class="course-content" @click="courseDetail">
           <van-row>
             <van-col span="7">
-            <van-row>
-              <span class="course-yearTime">20-11-09</span>
+              <van-row>
+                <span class="course-yearTime">20-11-09</span>
               </van-row>
               <van-row>
-              <span class="course-time">14:34:24</span>
+                <span class="course-time">14:34:24</span>
               </van-row>
-             </van-col>
-             <van-col span="1">
-             <van-image
+            </van-col>
+            <van-col span="1">
+              <van-image
                 src="../../../static/images/user.png"
                 width="30%"
                 radius="8px"
@@ -22,41 +21,103 @@
                 fit="cover"
                 @click="articleDetail"
               />
-              
-             </van-col>
-  
-             <van-col span="16">
-               <van-col span="17">
-               <span class="course-map">垃圾运输中</span>
-               </van-col>
-               <van-col>
-               <span class="course-pushBut">提交订单</span>
-               </van-col>
-                 <van-row>      
-                    <van-uploader >
-                      <van-button icon="photo" type="primary" custom-class="upImage"></van-button>
-                   </van-uploader>
-                   <van-uploader >
-                      <van-button icon="photo" type="primary" custom-class="upImage"></van-button>
-                   </van-uploader>
-                   <van-uploader >
-                      <van-button icon="photo" type="primary" custom-class="upImage"></van-button>
-                   </van-uploader>
-                   <van-uploader >
-                      <van-button icon="photo" type="primary" custom-class="upImage"></van-button>
-                   </van-uploader>
-                 </van-row>
-                  <span class="course-map">垃圾运输中</span>
+            </van-col>
+
+            <van-col span="16">
+              <van-col span="17">
+                <span class="course-map">垃圾运输中</span>
               </van-col>
+              <van-col>
+                <span class="course-pushBut">提交订单</span>
+              </van-col>
+              <van-row>
+                <van-uploader>
+                  <van-button
+                    icon="photo"
+                    type="primary"
+                    custom-class="upImage"
+                  ></van-button>
+                </van-uploader>
+                <van-uploader>
+                  <van-button
+                    icon="photo"
+                    type="primary"
+                    custom-class="upImage"
+                  ></van-button>
+                </van-uploader>
+                <van-uploader>
+                  <van-button
+                    icon="photo"
+                    type="primary"
+                    custom-class="upImage"
+                  ></van-button>
+                </van-uploader>
+                <van-uploader>
+                  <van-button
+                    icon="photo"
+                    type="primary"
+                    custom-class="upImage"
+                  ></van-button>
+                </van-uploader>
+              </van-row>
+              <span class="course-map">垃圾运输中</span>
+            </van-col>
           </van-row>
-        </div>  
+        </div>
+      </div>
+      <div class="course-list">
+        <div class="course-card">
+          <div class="course-content" @click="courseDetail">
+            <van-row>
+              <van-col span="7">
+                <van-row>
+                  <span class="course-yearTime">20-11-09</span>
+                </van-row>
+                <van-row>
+                  <span class="course-time">14:34:24</span>
+                </van-row>
+              </van-col>
+              <van-col span="1">
+                <van-image
+                  src="../../../static/images/user.png"
+                  width="30%"
+                  radius="8px"
+                  height="72px"
+                  fit="cover"
+                  @click="articleDetail"
+                />
+              </van-col>
+
+              <van-col span="16">
+                <van-col span="17">
+                  <span  class="course-order">正在前往目的地</span>
+                </van-col>
+                <van-col>
+                  <span class="course-pushBut">提交订单</span>
+                </van-col>
+               
+                <van-col span="24" >
+                <div class="course-bottomcolor">
+                <div>
+                 <van-col class="course-orderinformation" span="16">订单地址：区、街道</van-col>
+                 <van-col class="course-ordermap" sapn="16">上海市奉贤区南桥镇环城东路664号</van-col>
+                </div>
+                <div style="height: 2px; width: 100%"></div>
+                <div class="course-bottomcolorline">
+                 <van-col class="course-orderinformation" span="16">联系人</van-col>
+                 <van-col class="course-ordermap" sapn="16">李二狗<span class="course-ordermap">电话；联系：176****0276</span></span></van-col>
+                 </div>
+                </div>
+                </van-col>
+                
+              </van-col>
+            </van-row>
+          </div>
+        </div>
       </div>
     </div>
-    <div style="height: 5px; width: 100%"></div>
-    
-    
+    <div style="height: 2px; width: 100%"></div>
   </div>
-  
 </template>
 
 <script>
@@ -83,7 +144,6 @@ export default {
     // let app = getApp()
   },
 };
-
 </script>
 
 <style scoped>
@@ -125,9 +185,24 @@ export default {
 }
 .course-map {
   width: 100%;
- 
   font-size: 13px;
   color: rgba(1, 1, 1, 0.5);
+}
+.course-ordermap{
+   width: 100%;
+  font-size: 9px;
+  color: rgba(1, 1, 1, 0.5);
+}
+.course-order {
+  width: 100%;
+  font-size: 15px;
+  color: rgba(1, 1, 1, 0.9);
+}
+.course-orderinformation{
+  
+  width: 100%;
+  font-size: 11px;
+  color: rgba(1, 1, 1, 0.7);
 }
 .course-mapBottom {
   width: 100%;
@@ -145,12 +220,12 @@ export default {
 }
 .course-pushBut {
   padding: 4px;
-  
+
   color: aliceblue;
   font-size: 12px;
-  background-color: goldenrod;
-
+  background-color: sienna;
 }
+
 .discount-price {
   color: red;
 }
@@ -164,11 +239,25 @@ export default {
   background-color: rgba(255, 255, 255, 0.883);
   border-radius: 15px;
 }
->>>.upImage {
- background-color: gainsboro;
- border-color: white;
- width: 50px;
- margin-right: 5px;
- margin-top: 7px;
+>>> .upImage {
+  background-color: gainsboro;
+  border-color: white;
+  width: 50px;
+  margin-right: 5px;
+  margin-top: 7px;
+}
+.course-bottomcolor{
+  padding-top: 5px;
+  padding-left: 5px;
+  border-radius: 5px;
+  height: 60px;
+  width: 95%;
+  background-color: #f3f3f3e8;
+}
+.course-bottomcolorline{
+  padding-top: 10px;
+  height: 100%;
+  width: 100%;
+  background-color: #f3f3f3e8;
 }
 </style>
