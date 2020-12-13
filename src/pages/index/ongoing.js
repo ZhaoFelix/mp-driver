7f46f6ef30d1226332386a2be09752d68cbd9c5d
zhaofelix <15021179915@163.com>
2020-12-13 20:59:43 +0800

    fix:修改认证问题

--- src/pages/index/ongoing.js
+++ src/pages/index/ongoing.js
@@ -2,7 +2,7 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-11 09:17:51
- * @LastEditTime: 2020-12-13 20:28:39
+ * @LastEditTime: 2020-12-13 20:48:36
  * @FilePath: /mp-driver/src/pages/index/ongoing.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
@@ -93,7 +93,7 @@ export default {
                 return
               }
                 // 判断是否已认证
-              else  if (dataArr[0].user_type === null) {
+              else  if (dataArr[0].user_type === null || dataArr[0].user_type == 0) {
                   //  存储用户ID和用户类型
                   this.$store.commit("setUserID",dataArr[0].user_id)
                   this.$store.commit("setUserType",dataArr[0].user_type)
