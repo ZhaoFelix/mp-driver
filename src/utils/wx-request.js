d5b25f408ea5b82582aff35f65c61f2d27ac749c
zhaofelix <15021179915@163.com>
2021-03-23 14:58:38 +0800

    fix:增加接口验证

--- src/utils/wx-request.js
+++ src/utils/wx-request.js
@@ -2,15 +2,23 @@
  * @Author: Felix
  * @Email: felix@qingmaoedu.com
  * @Date: 2020-12-11 14:12:48
- * @LastEditTime: 2020-12-20 20:21:18
+ * @LastEditTime: 2021-03-23 14:54:21
  * @FilePath: /mp-driver/src/utils/wx-request.js
  * @Copyright © 2019 Shanghai Qingmao Network Technology Co.,Ltd All rights reserved.
  */
-import { requestUrl } from "../../config/options";
+import { 
+  requestUrl,
+  authKey,
+  authSecret,
+  authExpire } from "../../config/options";
 const host =
   process.env.NODE_ENV == "development"
     ? "http://localhost:3000/v1.0"
     : requestUrl;
+const jwt = require("jsonwebtoken");
+let token = jwt.sign({ secret: authSecret }, authKey, {
+  expiresIn: authExpire,
+});
 function request(url, method, data, header = {}) {
   wx.showLoading({
     title: "加载中",
@@ -20,8 +28,9 @@ function request(url, method, data, header = {}) {
       url: host + url,
       method: method,
       data: data,
-      headers: {
+      header: {
         "content-type": "application/json",
+        Authorization: "Bearer " + token,
       },
       success: function (res) {
         wx.hideLoading();
